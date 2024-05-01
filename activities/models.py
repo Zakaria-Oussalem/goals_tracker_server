35dff0343bc13a2d2cad11cea1dbe0446da7386e
ZakariaOussalem <Zakaria.oussalem.pro@gmail.com>
2024-05-01 08:26:33 +0200

    init app and models

---
+++ activities/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+DAY_TYPE_CHOICES = [
+    ("weekday", "weekday"),
+    ("weekend", "weekend"),
+    ("holiday", "holiday"),
+    ("vacation", "vacation"),
+]
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    insertion_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Rule(models.BaseModel):
+    name = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    reward = models.FloatField()
+    punishment = models.FloatField()
+    insertion_timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Logs(models.BaseModel):
+    activity = models.ForeignKey(Rule, on_delete=models.CASCADE)
+    timestamp = models.DateField()
+    insertion_timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class BaseValues(models.BaseModel):
+    day_type = models.CharField(
+        max_length=100, choices=DAY_TYPE_CHOICES, default="weekday"
+    )
+    base = models.FloatField()
+    insertion_timestamp = models.DateTimeField(auto_now_add=True)
